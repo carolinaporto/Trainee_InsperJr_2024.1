55ebf73fdbc878e8c68cb68c4b6e17272d03c6fd
Maria Carolina Porto <mariacp9@al.insper.edu.br>
2024-04-27 12:37:50 -0300

    fotos e reviews concluido

--- js/fotos-reviews.js
+++ js/fotos-reviews.js
@@ -37,5 +37,46 @@ document.addEventListener('DOMContentLoaded', function() {
         carouselInner.style.transform = `translateX(0px)`; // Volta para a posição inicial
       }
     }
+
+
+    // Carrossel 2 -> reviews 
+    const volt = document.querySelector('.voltar');
+    const avanc = document.querySelector('.avancar');
+    const carrosselInner = document.querySelector('.carrossel-inner');
+    const carrosselItems = document.querySelectorAll('.carrossel-item');
+  
+    let IndexAtual = 0;
+  
+    volt.addEventListener('click', function() {
+      if (IndexAtual === 0) {
+        return; // Retorna se estiver na primeira imagem
+      }
+      IndexAtual--;
+      updateCarrossel();
+    });
+  
+    avanc.addEventListener('click', function() {
+      if (IndexAtual === carrosselItems.length - 3) {
+        IndexAtual = 0; // Reinicia o carrossel
+        updateCarrossel();
+        return;
+      }
+      IndexAtual++;
+      updateCarrossel();
+    });
+  
+    function updateCarrossel() {
+      const largura_item = carrosselItems[0].offsetWidth + 10; // Largura da imagem + espaçamento
+      const largura_container = carrosselInner.offsetWidth;
+      const Imagens_visiveis = Math.floor(largura_container / largura_item); // Número de imagens visíveis na tela
+      const off_set = -IndexAtual * largura_item;
+      carrosselInner.style.transform = `translateX(${off_set}px)`;
+  
+      // Verifica se a última imagem está completamente visível e alinhada com a borda direita da tela
+      if (IndexAtual + Imagens_visiveis >= carrosselItems.length && off_set + largura_container >= carrosselItems.length * largura_item) {
+        IndexAtual = 0; // Reinicia o carrossel
+        carrosselInner.style.transform = `translateX(0px)`; // Volta para a posição inicial
+      }
+    }
   });
   
